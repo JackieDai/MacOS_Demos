b12693df89fe03a06c7ade5e0e3163465e917ef9
lingxiao <13656261355@163.com>
2024-12-23 10:45:28 +0800

    init

---
+++ MacOS_Demos/MacOS_Demos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacOS_Demos
+//
+//  Created by lingxiao on 2024/12/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
