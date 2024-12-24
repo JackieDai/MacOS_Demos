463167cb54976a7f91028e6285abe8658324d09b
lingxiao <13656261355@163.com>
2024-12-24 08:47:44 +0800

    更改默认窗口尺寸

--- MacOS_Demos/MacOS_Demos/AppDelegate.swift
+++ MacOS_Demos/MacOS_Demos/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        modifyWindowSize()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -28,3 +29,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 }
 
+
+extension AppDelegate {
+    // modify the window default size
+    func modifyWindowSize() {
+        
+        guard let screen = NSScreen.main else {
+            print("screen is unavailable")
+            return
+        }
+        
+        /**
+         NSApplication.shared.mainWindow 只有在窗口被激活时才会返回正确的值。如果窗口还没有成为主窗口，mainWindow 的值将为 nil。
+         */
+//        guard let mainWindow = NSApplication.shared.mainWindow else {
+//            print("mainWindow is unavailable")
+//            return
+//        }
+        
+        guard let mainWindow = NSApp.windows.first else {
+            print("mainWindow is unavailable")
+            return
+        }
+        
+        let screenFrame = screen.visibleFrame
+        
+        let width = 800.0
+        let height = 600.0
+        
+        let x = NSMidX(screenFrame) - width * 0.5
+        let y = NSMidY(screenFrame) - height * 0.5
+
+        // 创建窗口尺寸相对屏幕中心的位置
+        
+        let frame = NSMakeRect(x, y, width, height)
+
+        mainWindow.setFrame(frame, display: true)
+
+    }
+}
